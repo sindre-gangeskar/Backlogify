a4cb2961929115a437bd395552da4d412237380b
sindre-gangeskar~ <sindre.gangeskar2710@outlook.com>
2024-06-26 01:31:02 +0200

    Added backlog directory creation fail-safe

--- server/routes/index.js
+++ server/routes/index.js
@@ -8,6 +8,10 @@ const fs = require('fs');
 
 router.use(jsend.middleware);
 router.get('/:steamid', async function (req, res, next) {
+  const backlogPath = path.resolve(__dirname, '../data/backlog');
+  if (!backlogPath)
+    fs.mkdirSync(path.resolve(__dirname, '../data/backlog'));
+
   const id = req.params.steamid;
   try {
     const exists = fs.existsSync(path.resolve(__dirname, `../data/${id}.json`));
@@ -59,6 +63,23 @@ router.get('/:steamid', async function (req, res, next) {
 
 });
 
+router.get('/backlog/:steamid', async function (req, res, next) {
+  const id = req.params.steamid;
+  try {
+    const exists = fs.existsSync(path.resolve(__dirname, `../data/backlog/${id}.json`));
+
+    if (exists) {
+      const data = JSON.parse(fs.readFileSync(path.resolve(__dirname, `../data/backlog/${id}.json`), 'utf-8'));
+      return res.jsend.success({ appids: data });
+    }
+    else {
+      res.jsend.fail({ data: 'Could not find games associates with steam id' });
+    }
+  } catch (error) {
+    return res.jsend.error({ message: 'something went wrong' });
+  }
+})
+
 router.post('/backlog', async function (req, res, next) {
   const { name, appid, playtime_forever, steamid } = req.body;
   let data = [];
@@ -78,31 +99,21 @@ router.post('/backlog', async function (req, res, next) {
   return res.jsend.success({ data: { name, appid }, message: 'Added game to backlog' });
 })
 
-router.get('/backlog/:steamid', async function (req, res, next) {
-  const id = req.params.steamid;
-  try {
-    const exists = fs.existsSync(path.resolve(__dirname, `../data/backlog/${id}.json`));
-
-    if (exists) {
-      const data = JSON.parse(fs.readFileSync(path.resolve(__dirname, `../data/backlog/${id}.json`), 'utf-8'));
-      return res.jsend.success({ appids: data });
-    }
-    else {
-      res.jsend.fail({ data: 'Could not find games associates with steam id' });
-    }
-  } catch (error) {
-    return res.jsend.error({ message: 'something went wrong' });
-  }
-})
-
 router.delete('/backlog', async function (req, res, next) {
   const { appid, steamid } = req.body;
-  const data = JSON.parse(fs.readFileSync(path.resolve(__dirname, `../data/backlog/${steamid}.json`)));
-  const dataToKeep = data.filter(x => x.appid !== +appid);
+  try {
+    const exists = fs.existsSync(path.resolve(__dirname, `../data/backlog/${steamid}.json`));
 
-  console.log(req.body);
+    if (exists) {
+      const data = JSON.parse(fs.readFileSync(path.resolve(__dirname, `../data/backlog/${steamid}.json`)));
+      const dataToKeep = data.filter(x => +x.appid !== +appid);
+      fs.writeFileSync(path.resolve(__dirname, `../data/backlog/${steamid}.json`), JSON.stringify(dataToKeep, null, 2));
+      return res.jsend.success({ data: { appid }, message: 'Successfully removed game from backlog' });
+    }
 
-  fs.writeFileSync(path.resolve(__dirname, `../data/backlog/${steamid}.json`), JSON.stringify(dataToKeep, null, 2));
-  res.jsend.success({ data: { appid }, message: 'Successfully removed game from backlog' });
+    else return res.jsend.fail({ data: null, message: 'No backlog for current user exists' });
+  } catch (error) {
+    return res.jsend.fail({ data: { error: error, message: error.statusText } });
+  }
 })
 module.exports = router;
