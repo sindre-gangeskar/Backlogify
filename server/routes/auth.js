c7bf9056dc9a33daac11df903f6d671284251fca
sindre-gangeskar <sindre.gangeskar2710@outlook.com>
2024-08-06 12:34:35 +0200

    Add withCredentials in axios request on server. Add session regeneration and then save afterwards upon successful login

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -17,13 +17,18 @@ router.get('/login/authenticated', async function (req, res, next) {
     try {
         const steam = await steamSignIn.verifyLogin(req.url);
         const steamid64 = steam.getSteamID64();
-        const response = await axios.get(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.KEY}&steamids=${steamid64}`);
+        const response = await axios.get(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=${process.env.KEY}&steamids=${steamid64}`, {withCredentials: true});
         const { personaname, avatarfull } = response.data.response.players[ 0 ];
 
+        
+        req.session.regenerate(err => {
+            if (err) console.error(err);
+        })
+        
         req.session.user = { steamid64, personaname, avatarfull };
+
         req.session.save(() => {
             console.log('Session saved sucessfully!', req.session);
-            
         })
 
         res.redirect(process.env.CLIENT_BASEURL);
