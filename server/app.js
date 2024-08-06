4e97c38e13b51c9d944dfffb91dbe3ae472d8f82
sindre-gangeskar <sindre.gangeskar2710@outlook.com>
2024-08-06 17:32:58 +0200

    Create data directory if it does not exist

--- server/app.js
+++ server/app.js
@@ -19,8 +19,12 @@ var authRouter = require('./routes/auth');
 
 const dbDirectory = path.resolve(__dirname, './db');
 if (!fs.existsSync(dbDirectory)) {
-    fs.mkdirSync(path.join(__dirname, 'db'));
+    fs.mkdirSync(path.join(__dirname, 'db'), {recursive: true});
 }
+
+const backlogPath = path.join(__dirname, 'data/backlog');
+if (!fs.existsSync(backlogPath))
+    fs.mkdirSync(path.join(__dirname, 'data/backlog'), {recursive: true})
 
 var app = express();
 
